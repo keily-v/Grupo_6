2b02351d0f3613ce893c0d73cf40b589d6191a4f
Keily Vallejo <kmvallejo2@espe.edu.ec>
2021-05-26 13:00:07 -0500

    funcion vali

--- operacioness.cpp
+++ operacioness.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int validar(int n){
+int validar(int &n){
 do{
 cout<<"Ingrese un numero que desee entre 10 y 20: ";
 cin >> n;
 if(n>=10 && n<=20) {
     cout<<"Numero valido \n";
+
 }
 else{
-    cout<<"El numero ingresado es incorrecto!! \n";
-    cout<<"Por favor ingrese un numero entre 10 y 20 \n";
+    cout<<"Numero no valido! \n";
     system("PAUSE");
-    system("cls");//Limpiar pantalla
+    system("cls");
 }
 }while ((n<10)||(n>20));
-        return(n);
+    return(n);
 }
 
 int  main(){
     int n;
-    cout<<validar(n);
+    validar(n);
 }
