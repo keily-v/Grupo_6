634c72620773fcd9c90c97d67f79f830a78c5e4a
Keily Vallejo <kmvallejo2@espe.edu.ec>
2021-05-26 12:17:22 -0500

    Funcion validar un numero

--- operacioness.cpp
+++ operacioness.cpp
@@ -1,19 +1,21 @@
-
 #include <iostream>
 using namespace std;
 
-int validar (int n){
-    cout<<"Ingrese un numero: ";cin>>n;
-    if (n>10 && n<20){
-        cout<<"Numero Valido";}
-    else{
-    cout<<"Numero Invalido";}
-
+int validar(int n){
+do{
+cout<<"Ingrese un numero que desee entre 10 y 20: ";
+cin >> n;
+if(n>=10 && n<=20) {
+    cout<<"Numero valido \n";
 }
-
+else{
+    cout<<"El numero ingresado es incorrecto!! \n";
+    cout<<"Por favor ingrese un numero entre 10 y 20 \n";
+}
+}while (n>=10 && n<=20);
+return (n);
+}
 int  main(){
     int n=0;
-    //
     validar(n);
 }
-
